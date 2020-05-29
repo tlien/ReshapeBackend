cdb4cb9bab08c2fe37d0363471c31d64b5e6b7f8
twast92 <twast.92@gmail.com>
2020-05-29 14:17:53 +0200

    lol

--- src/AccountService/Program.cs
+++ src/AccountService/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Npgsql;
+
 using Reshape.AccountService.Infrastructure;
 using Reshape.Common.Extensions;
 
