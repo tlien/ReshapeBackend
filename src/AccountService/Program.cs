bfb6ad65c9ca2e73ed684a7fd009948403eb4ae7
twast92 <twast.92@gmail.com>
2020-06-06 17:00:31 +0200

    Fix appName to include both root namespace and service namespace

--- src/AccountService/Program.cs
+++ src/AccountService/Program.cs
@@ -16,7 +16,7 @@ namespace Reshape.AccountService
     public static class Program
     {
         public static readonly string Namespace = typeof(Program).Namespace;
-        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.') - 1);
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
 
         public static int Main(string[] args)
         {
