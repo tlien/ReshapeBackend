ce6f5af4edb186df75c5a0ab96e08860fb4a725a
twast92 <twast.92@gmail.com>
2020-06-08 12:52:43 +0200

    Add logging to ApiGateway Make host building adhere to current .net core standards (IWebHost is only for backwards compat, use generic IHost) Add Reshape root namespace

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,31 +1,74 @@
+using System;
 using System.IO;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
+using Serilog;
 
-namespace ApiGateway
+namespace Reshape.ApiGateway
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static readonly string Namespace = typeof(Program).Namespace;
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+
+        public static int Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var configuration = GetConfiguration();
+            Log.Logger = CreateSerilogLogger(configuration);
+
+            try
+            {
+                Log.Information("Configuring web host ({ApplicationContext})...", AppName);
+                var host = CreateHostBuilder(args).Build();
+
+                Log.Information("Starting web host ({ApplicationContext})...", AppName);
+                host.Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", AppName);
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            var builder = WebHost.CreateDefaultBuilder(args);
-            builder.ConfigureServices(s => s.AddSingleton(builder))
-                    .UseKestrel()
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseStartup<Startup>();
-            var host = builder.Build();
-            return host;
-        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, cfg) =>
+                {
+                    // Add Ocelot/Swagger config
+                    cfg.AddJsonFile($"configuration.{ctx.HostingEnvironment.EnvironmentName}.json", optional: false);
+                })
+                .ConfigureWebHost(webBuilder =>
+                {
+                    webBuilder
+                        .UseKestrel()
+                        .UseStartup<Startup>();
+                })
+                .UseSerilog();
+
+        private static IConfiguration GetConfiguration() =>
+           new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+        private static ILogger CreateSerilogLogger(IConfiguration configuration) =>
+            new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.WithProperty("ApplicationContext", AppName)
+                .Enrich.FromLogContext()
+                // .WriteTo.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
     }
 }
--- src/ApiGateway/Startup.cs
+++ src/ApiGateway/Startup.cs
@@ -1,36 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-namespace ApiGateway
+namespace Reshape.ApiGateway
 {
     public class Startup
     {
-        public Startup(IWebHostEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", true, true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
-                .AddJsonFile($"configuration.{env.EnvironmentName}.json")
-                .AddEnvironmentVariables();
-
-            Configuration = builder.Build();
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -38,8 +22,10 @@ namespace ApiGateway
             services.AddSwaggerForOcelot(Configuration);
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, ILogger<Startup> logger)
         {
+
+            logger.LogInformation("Startup Configuring... woah!");
             app.UseSwaggerForOcelotUI();
             app.UseOcelot().Wait();
         }
