9fe58151b3842b5e04f5f8172be030c768efcf75
twast92 <twast.92@gmail.com>
2020-06-21 02:33:32 +0200

    More docs

--- src/Common/EventBus/Events/IIntegrationEvent.cs
+++ src/Common/EventBus/Events/IIntegrationEvent.cs
@@ -2,6 +2,12 @@ using System;
 
 namespace Reshape.Common.EventBus.Events
 {
+    /// <summary>
+    /// Base integration event contract.
+    /// Holds information used by the IntegrationEventLogService.
+    ///
+    /// **Not meant to be consumed!**
+    /// </summary>
     public interface IIntegrationEvent
     {
         Guid EventId { get; }
